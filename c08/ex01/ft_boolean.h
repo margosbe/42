495d3d3c677261fd5ee94b94852ccce0f9b09cba
margosbe <127108657+margosbe@users.noreply.github.com>
2025-03-12 11:10:10 +0000

    starting 8

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,14 @@
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+#define SUCCESS	0
+#define TRUE 1
+#define FALSE 0
+#define EVEN
+#define EVEN_MSG "I have a pair number of arguments.\n"
+#define ODD_MSG "I have an impair number of arguments.\n"
+
+int	ft_is_even(int nbr);
+void	ft_putstr(char *str);
+
+#endif
